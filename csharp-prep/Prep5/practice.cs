162d760122f2a7ad3c05c4fea5e0e14f2124b973
Intchilz <chi21012@byui.edu>
2023-05-03 18:13:13 +0200

    Added a practice C# file

---
+++ csharp-prep/Prep5/practice.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Blind
+{
+    public double _width= 1;
+    public double _height= 3;
+    public string _color;
+    
+    public double GetArea()
+    {
+        return _width * _height;
+    }
+}
